0aee7121dc446dce265c20b9fbf4af427d5da95c
aya25donia <aya2donia25@gmail.com>
2019-12-25 22:28:52 +0200

    add new changes

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,40 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import AOS from 'aos'
+import Vuelidate from 'vuelidate';
+import Toasted from 'vue-toasted';
+import 'aos/dist/aos.css'
+import VueApexCharts from 'vue-apexcharts'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import 'chart.js'
+import 'hchs-vue-charts'
+import VueFlashMessage from 'vue-flash-message'
+import InfiniteLoading from 'vue-infinite-loading';
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+
+
 Vue.config.productionTip = false
+Vue.use(require('vue-moment'));
+Vue.use(Vuelidate);
+Vue.use(Toasted, {})
+Vue.use(VueApexCharts);
+Vue.component('apexchart', VueApexCharts);
+Vue.use(VueAxios, axios);
+Vue.use(window.VueCharts);
+Vue.use(VueFlashMessage);
+Vue.use(InfiniteLoading, {});
 
 new Vue({
+  created() {
+    AOS.init();
+  },
   router,
   store,
   render: h => h(App)
